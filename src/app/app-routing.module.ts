32006406ebbce8b88460b17a8f0e51d98a9a18f0
DESKTOP-7G9H5SE\User <chattrakul.opyn@gmail.com>
2026-03-26 13:45:58 +0700

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ReceiveQueueComponent } from './receive-queue/receive-queue.component';
+import { ShowQueueComponent } from './show-queue/show-queue.component';
+import { ClearQueueComponent } from './clear-queue/clear-queue.component';
 
-const routes: Routes = [];
+const routes: Routes = [  {
+    path: '',
+    component: ReceiveQueueComponent,
+  },
+  {
+    path: 'show-queue',
+    component: ShowQueueComponent,
+  },
+  {
+    path: 'clear-queue',
+    component: ClearQueueComponent,
+  },];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { ReceiveQueueComponent } from './receive-queue/receive-queue.component';
+import { ShowQueueComponent } from './show-queue/show-queue.component';
+import { ClearQueueComponent } from './clear-queue/clear-queue.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ReceiveQueueComponent,
+    ShowQueueComponent,
+    ClearQueueComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule,
+    CommonModule,
+    HttpClientModule
   ],
   providers: [
     provideClientHydration()
